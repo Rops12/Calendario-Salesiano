e56c023f3191e89f84820320026a7e871489520f
Rops12 <matheus.oliveira.correia12@gmail.com>
2025-07-22 23:52:52 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ProtectedRoute } from "@/components/Auth/ProtectedRoute";
-import { CategoriesProvider } from "@/hooks/useCategories"; // Importado
+import { CategoriesProvider } from "@/hooks/useCategories.ts"; // CORREÇÃO AQUI
 import Index from "./pages/Index";
 import Auth from "./pages/Auth";
 import NotFound from "./pages/NotFound";
@@ -17,7 +17,6 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      {/* O CategoriesProvider envolve as rotas */}
       <CategoriesProvider>
         <BrowserRouter>
           <Routes>
