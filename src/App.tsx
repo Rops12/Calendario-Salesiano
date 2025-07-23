9a0ab801bd41692ad82a3fe1eabed2c16965faf6
Rops12 <matheus.oliveira.correia12@gmail.com>
2025-07-23 12:22:27 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ProtectedRoute } from "@/components/Auth/ProtectedRoute";
-import { CategoriesProvider } from "@/hooks/useCategories.ts"; // Correção da importação
+import { CategoriesProvider } from "@/hooks/useCategories.tsx"; // CORREÇÃO AQUI
 import Index from "./pages/Index";
 import Auth from "./pages/Auth";
 import NotFound from "./pages/NotFound";
